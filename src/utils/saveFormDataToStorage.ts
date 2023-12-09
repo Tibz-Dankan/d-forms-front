7a1de8f6b6b108407231b1eeea51502105c9a36b
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-12-09 05:29:19 +0300

    feat: save page data in the storage

---
+++ src/utils/saveFormDataToStorage.ts
@@ -0,0 +1,58 @@
+interface Data {}
+
+interface Props {
+  applicationForm: string;
+  category: string;
+  updateAt: string;
+  data: Data;
+}
+
+interface StorageData {
+  applicationForm: string;
+  updateAt: string;
+  categories: any;
+}
+
+export const saveFormDataToStorage = (props: Props) => {
+  const storageData = localStorage.getItem(props.applicationForm);
+
+  const applicationFormData =
+    storageData && (JSON.parse(storageData).applicationForm as StorageData);
+
+  //When storage is empty
+  if (!applicationFormData) {
+    const data = {} as StorageData;
+    data.applicationForm = props.applicationForm;
+    data.updateAt = props.updateAt;
+    data.categories = {};
+    data.categories[`${props.category}`] = props.data;
+    localStorage.setItem(props.applicationForm, JSON.stringify(data));
+    return;
+  }
+
+  //When storage is having data
+  applicationFormData.updateAt = props.updateAt;
+  applicationFormData.categories = {};
+  applicationFormData.categories[`${props.category}`] = props.data;
+  localStorage.setItem(
+    props.applicationForm,
+    JSON.stringify(applicationFormData)
+  );
+};
+
+interface getDataProps {
+  applicationForm: string;
+  category: string;
+}
+export const getDataFromStorage = (props: getDataProps) => {
+  const storageData = localStorage.getItem(props.applicationForm);
+
+  if (!storageData) return null;
+
+  const applicationFormData =
+    storageData && (JSON.parse(storageData) as StorageData);
+
+  if (!applicationFormData) return null;
+
+  return applicationFormData.categories[`${props.category}`];
+};
