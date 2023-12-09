3ddce7af184ab3f3a6a1f974c1c943c6deb01a4e
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-12-09 11:45:07 +0300

    feat: setup routes for the postgraduate application form

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ import { Notification } from "./UI/shared/Notifiication";
 import { hideCardNotification } from "./store/actions/notification";
 import { TNotificationState } from "./types/notification";
 import "./App.css";
-// import { Section1 } from "./pages/employment/Section1";
 import { EmploymentRouteController } from "./pages/employment/EmploymentRouteController";
+import { PostgraduateRouteController } from "./pages/postgraduate/PostgraduateRouteController";
 
 const App: React.FC = () => {
   const dispatch: any = useDispatch();
@@ -43,6 +43,10 @@ const App: React.FC = () => {
           )}
           <Routes>
             <Route path="/" element={<div>Home Page here</div>} />
+            <Route
+              path="/postgraduate/*"
+              element={<PostgraduateRouteController />}
+            />
             <Route
               path="/employment/*"
               element={<EmploymentRouteController />}
---
+++ src/pages/postgraduate/Section1.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Section1: React.FC = () => {
+  return <div>Section1</div>;
+};
