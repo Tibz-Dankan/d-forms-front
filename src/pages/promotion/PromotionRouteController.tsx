fe832b19afd045e5dc5abe15abf1dc088b1fac54
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-12-16 12:04:08 +0300

    feat: add section 7 to capture applicants research status

--- src/pages/promotion/PromotionRouteController.tsx
+++ src/pages/promotion/PromotionRouteController.tsx
@@ -5,6 +5,7 @@ import { Section3 } from "./Section3";
 import { Section4 } from "./Section4";
 import { Section5 } from "./Section5";
 import { Section6 } from "./Section6";
+import { Section7 } from "./Section7";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -39,6 +40,11 @@ export const PromotionRouteController: React.FC = () => {
       page: 6,
       element: <Section6 />,
       path: "/Section6",
+    },
+    {
+      page: 7,
+      element: <Section7 />,
+      path: "/Section7",
     },
   ];
 
